bba4e805f964a29ae79d7822ed83faa6bbcb0602
João Cláudio <leiloka46@gmail.com>
2024-12-30 16:05:23 -0300

    Main view controller

--- src/main/java/com/alexey/workshopjavafxjdbc/HelloController.java
+++ src/main/java/com/alexey/workshopjavafxjdbc/HelloController.java
@@ -1,14 +1,31 @@
 package com.alexey.workshopjavafxjdbc;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.MenuItem;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private MenuItem menuItemSeller;
+
+    @FXML
+    private MenuItem menuItemDepartment;
+
+    @FXML
+    private MenuItem menuItemAbout;
+
+    @FXML
+    public void onMenuItemSellerAction() {
+        System.out.println("onMenuItemSellerAction");
+    }
+
+    @FXML
+    public void onMenuItemDepartmentAction() {
+        System.out.println("onMenuItemDepartmentAction");
+    }
+
+    @FXML
+    public void onMenuItemAboutAction() {
+        System.out.println("onMenuItemAboutAction");
     }
 }
